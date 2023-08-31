aacb41e919669597e5d6085def042841fb2a2eee
Rafael Freitas de Carvalho <TJC0193813@aluno.firjansenaisesi.com.br>
2023-08-31 12:12:32 -0300

    criacao

--- js/main.js
+++ js/main.js
@@ -10,7 +10,20 @@ let Start = setInterval(()=>{
     // Add your methods for Sanddox here
     YeEngine.AlterThemeSystem(YeEngine.Theme.DarkTheme)
     YeEngine.AlterTheme(YeEngine.Theme.GreenTheme)
+    YeEngine.ModifyW(YeEngine.GetlementById("card","200vh"))
+    YeEngine.ModifyH(YeEngine.GetlementById("card","300h"))
+ 
 
     //End Your methods Here
     clearInterval(Start) //Dont erase this line
 },400)
+
+let Update = setInterval(() =>{
+    if(YeEngine.Questions_save[0] != undefined)
+
+{
+YeEngine.ModifyContentClass(document.getElementById("card"),"ola mundo")
+clearInterval(Update), YeEngine.Questions_save[0]
+
+}
+},600)
